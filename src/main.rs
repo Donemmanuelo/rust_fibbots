96c9ad3acf265eced898b16c538a68154ac5c6b6
Donemmanuelo <emmanuelodon943@gmail.com>
2025-02-26 16:04:44 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use tests::lib::output;
+
 fn main() {
-    println!("Hello, world!");
+    let y = output();
+    println!("{y}");
+}
+mod tests;
+#[cfg(test)]
+mod test {
+    use crate::tests::lib::output;
+
+    #[test]
+    fn test_main() {
+        use crate::main;
+        assert_eq!(main(), ());
+    }
+    #[test]
+    fn test_output() {
+        
+        assert_ne!(output(), "Hello, Rust!");
+        assert_eq!(output(), "Hello, World!");
+    }
 }
---
+++ src/tests/lib.rs
@@ -0,0 +1,6 @@
+pub fn output() -> String {
+    let x = "Hello,";
+    let y = "World!";
+    let result = x.to_owned() + " " + y;
+    result
+}
