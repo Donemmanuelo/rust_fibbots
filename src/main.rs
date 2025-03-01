6d34ea94bf511d59c6e2a49e5eefb238c9b94263
Donemmanuelo <emmanuelodon943@gmail.com>
2025-03-01 21:36:45 +0100

    just made some little changes

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Get environment variables
     //let repo_owner = env::var("GITHUB_ENV").expect("GITHUB_REPOSITORY_OWNER not set");
+    let repo_owner = env::var("GITHUB_REPOSITORY").expect("GITHUB_REPOSITORY_OWNER not set");
     let repo_name = env::var("GITHUB_REPOSITORY").expect("GITHUB_REPOSITORY_NAME not set");
     let pr_number = env::var("GITHUB_EVENT_NAME").expect("PR_NUMBER not set");
     let token = env::var("INPUT_GITHUB_TOKEN").expect("GITHUB_TOKEN not set");
@@ -38,8 +39,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Fetch the pull request diff
     let diff_url = format!(
-        "https://api.github.com/repos/{}/pulls/{}",
-         repo_name, pr_number
+        "https://api.github.com/repos/{}/{}/pulls/{}",
+         repo_owner, repo_name, pr_number
     );
 
    
--- src/tests/lib.rs
+++ src/tests/lib.rs
@@ -6,7 +6,7 @@ pub fn fibbonnacci(c: u128) -> BigInt {
   match  c {
     0 => 0.to_bigint().expect("invalid input"),
     1=> 1.to_bigint().expect("invalid input"),
-  _ => fibbonnacci(c - 1) + fibbonnacci(c -1)
+  _ => fibbonnacci(c - 2) + fibbonnacci(c -1)
   }
 
 }
