190eaedabef4b89f74fb4e0501ee9ba0a3b05352
Donemmanuelo <emmanuelodon943@gmail.com>
2025-03-03 07:34:16 +0100

    just made some little changes

---
+++ target/debug/deps/fibbot-c9b0427904a52816.d
@@ -0,0 +1,7 @@
+/home/don/rust-programming/fibbot/target/debug/deps/libfibbot-c9b0427904a52816.rmeta: src/main.rs src/tests/mod.rs src/tests/lib.rs
+
+/home/don/rust-programming/fibbot/target/debug/deps/fibbot-c9b0427904a52816.d: src/main.rs src/tests/mod.rs src/tests/lib.rs
+
+src/main.rs:
+src/tests/mod.rs:
+src/tests/lib.rs:
